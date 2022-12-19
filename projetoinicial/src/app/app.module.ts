80811d64aa41e26ddc7dedf809c1c88188451bd2
Paula Leticia <paula.leticia.braz@gmail.com>
2022-12-19 11:55:13 -0300

    criando as pasta home e atualizando as configurações da pasta

--- projetoinicial/src/app/app.module.ts
+++ projetoinicial/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CadastroComponent } from './cadastro/cadastro.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
